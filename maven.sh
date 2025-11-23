1d0c5db710c417af339c1b5f112d0018962b1d84
Sahithya Priya <sahithya3098@gmail.com>
2025-11-23 19:23:36 +0530

    Create maven.sh

---
+++ maven.sh
@@ -0,0 +1,6 @@
+# to install maven use this script
+#-y is to say yes automatically to all the installation prompts looking for permission
+#mvn -v is to see the version. 
+sudo apt update -y
+sudo apt install maven -y 
+mvn -v 
